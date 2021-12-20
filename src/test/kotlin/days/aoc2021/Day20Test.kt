fbdbf95d7ddaf0fbfc3053f36f51f2451107bdf0
Brian Dupuis <brian.dupuis@genesys.com>
2021-12-20 10:22:24 -0500

    Day 20

---
+++ src/test/kotlin/days/aoc2021/Day20Test.kt
@@ -0,0 +1,35 @@
+package days.aoc2021
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+
+class Day20Test {
+    private val day = Day20()
+    private val inputLines = """
+        ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+        #..#.
+        #....
+        ##..#
+        ..#..
+        ..###
+    """.trimIndent().lines()
+
+    @Test
+    fun testPartOne() {
+        val pair = day.parseInput(inputLines)
+        val image = pair.second.enhance(pair.first, 2)
+
+        MatcherAssert.assertThat(day.countLightPixels(image), `is`(35))
+    }
+
+    @Test
+    fun testPartTwo() {
+        val pair = day.parseInput(inputLines)
+        val image = pair.second.enhance(pair.first, 50)
+
+        MatcherAssert.assertThat(day.countLightPixels(image), `is`(3351))
+    }
+
+}
