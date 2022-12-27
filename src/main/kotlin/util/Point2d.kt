1bb611b3eeba4645049f201cab4de3cbd8457673
Brian Dupuis <brian.dupuis@genesys.com>
2022-12-27 08:41:14 -0500

    Util

--- src/main/kotlin/util/Point2d.kt
+++ src/main/kotlin/util/Point2d.kt
@@ -6,4 +6,22 @@ data class Point2d(val x: Int, val y: Int) {
     fun distanceTo(other: Point2d): Int {
         return abs(other.x - x) + abs(other.y - y)
     }
+
+    operator fun plus(other: Point2d): Point2d {
+        return Point2d(x + other.x, y + other.y)
+    }
+
+    fun neighbors() = listOf(
+        copy(x = x + 1),
+        copy(x = x - 1),
+        copy(y = y + 1),
+        copy(y = y - 1),
+    )
+
+}
+
+data class Point2dl(val x: Long, val y: Long) {
+    fun distanceTo(other: Point2dl): Long {
+        return abs(other.x - x) + abs(other.y - y)
+    }
 }
